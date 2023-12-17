664e247ffda34e92f303e307dc7ae5091caef835
Yanis Bouchelaghem <bouchelaghemyanis7@gmail.com>
2023-12-17 19:10:17 +0100

    Implemented multiple seed generation for each run of the KNN evaluation.

--- RF/main.cpp
+++ RF/main.cpp
@@ -6,11 +6,13 @@
 #include "Metrics.h"
 #include "KNNEval.h"
 #include "KMeansEval.h"
+#include <random>
 
 int main()
 {
+	const std::string REPRESENTATION = "SA";
 	std::filesystem::path currentPath = std::filesystem::current_path();
-	DatasetLoader datasetLoader{ currentPath.string() + "\\..\\images\\E34", 7};
+	DatasetLoader datasetLoader{ currentPath.string() + "\\..\\images\\" + REPRESENTATION, 7};
 	//KNNAlgorithm knn(datasetLoader);
 	//int result = knn.PredictOne(10, datasetLoader.GetEvaluationData()[9]);
 	//std::vector<int> results = knn.Predict(10, datasetLoader.GetEvaluationData());
@@ -50,11 +52,75 @@ int main()
 		kMeansFitTime.push_back(time);
 	}
 
+	//Generate 100 ints randomly.
+	std::mt19937 mt(10);
+	std::vector<int> seedListKnn;
+	for (int i = 0; i < 100; ++i)
+	{
+		seedListKnn.push_back(mt());
+	}
+	std::vector<Metrics> knnMeansByK;
 	//Do the knn predictions with different k values and measure the time.
 	for (int k : knnKList)
 	{
-		KNNEval knnEval(datasetLoader);
-		knnMetrics.push_back(knnEval.Evaluate(k));
+		std::vector<Metrics> metricsForK;
+		for (int seed : seedListKnn)
+		{
+			DatasetLoader datasetLoader{ currentPath.string() + "\\..\\images\\" + REPRESENTATION, seed};
+			KNNEval knnEval(datasetLoader);
+			metricsForK.push_back(knnEval.Evaluate(k));
+		}
+		Metrics meanKnnMetrics;
+		//Initialize knn metrics to 0
+		meanKnnMetrics.accuracy = 0;
+		meanKnnMetrics.time = 0;
+		meanKnnMetrics.classesPrecisionRecallF1.resize(datasetLoader.GetClassCount());
+
+		for (PrecisionRecallF1& precisionRecallF1 : meanKnnMetrics.classesPrecisionRecallF1)
+		{
+			precisionRecallF1.f1Score = 0;
+			precisionRecallF1.precision = 0;
+			precisionRecallF1.recall = 0;
+		}
+		for (const Metrics& metrics : metricsForK)
+		{
+			meanKnnMetrics.accuracy += metrics.accuracy;
+			meanKnnMetrics.time += metrics.time;
+			for (int i = 0; i < meanKnnMetrics.classesPrecisionRecallF1.size(); ++i)
+			{
+				meanKnnMetrics.classesPrecisionRecallF1[i].precision += metrics.classesPrecisionRecallF1[i].precision;
+				meanKnnMetrics.classesPrecisionRecallF1[i].recall += metrics.classesPrecisionRecallF1[i].recall;
+				meanKnnMetrics.classesPrecisionRecallF1[i].f1Score += metrics.classesPrecisionRecallF1[i].f1Score;
+			}
+		}
+		//Divide the values for knn
+		int divider = metricsForK.size();
+		meanKnnMetrics.accuracy = meanKnnMetrics.accuracy / divider;
+		meanKnnMetrics.time = meanKnnMetrics.time / divider;
+		for (PrecisionRecallF1& precisionRecallF1 : meanKnnMetrics.classesPrecisionRecallF1)
+		{
+			precisionRecallF1.f1Score = precisionRecallF1.f1Score / divider;
+			precisionRecallF1.precision = precisionRecallF1.f1Score / divider;
+			precisionRecallF1.recall = precisionRecallF1.f1Score / divider;
+		}
+		knnMeansByK.push_back(meanKnnMetrics);
+	}
+
+	for (int i = 0; i < knnMeansByK.size(); ++i)
+	{
+		Metrics metrics = knnMeansByK[i];
+		std::cout << "\n\nMean KNN metrics for k = " << knnKList[i] << " :\n";
+		std::cout << "Confusion matrix :\n";
+		PrintConfusionMatrix(metrics.confusionMatrix);
+		std::cout << "Accuracy : " << metrics.accuracy << "%; " << "time : " << metrics.time * 1000 << " ms\n";
+		for (int j = 0; j < metrics.classesPrecisionRecallF1.size(); ++j)
+		{
+			std::cout << "class " << j + 1 << "\n";
+			std::cout << "Precision : " << metrics.classesPrecisionRecallF1[j].precision * 100 << "%\t";
+			std::cout << "Recall : " << metrics.classesPrecisionRecallF1[j].recall * 100 << "%\t";
+			std::cout << "f1 score : " << metrics.classesPrecisionRecallF1[j].f1Score * 100 << "%\t";
+			std::cout << "\n";
+		}
 	}
 	// Calculate the mean for knn and kmeans
 	Metrics meanKnnMetrics;
@@ -139,21 +205,21 @@ int main()
 
 	//Print everything
 
-	for (int i = 0; i < knnMetrics.size(); ++i)
-	{
-		Metrics metrics = knnMetrics[i];
-		std::cout << "\n\nKNN metrics for k = " << knnKList[i] << " :\n";
-		std::cout << "Confusion matrix :\n";
-		PrintConfusionMatrix(metrics.confusionMatrix);
-		std::cout << "Accuracy : " << metrics.accuracy << "%; " << "time : " << metrics.time * 1000 << " ms\n";
-		for (int j = 0; j < metrics.classesPrecisionRecallF1.size(); ++j)
-		{
-			std::cout << "class " << j + 1 << "\n";
-			std::cout << "Precision : " << metrics.classesPrecisionRecallF1[j].precision * 100 << "%\t";
-			std::cout << "Recall : " << metrics.classesPrecisionRecallF1[j].recall * 100 << "%\t";
-			std::cout << "f1 score : " << metrics.classesPrecisionRecallF1[j].f1Score * 100 << "%\t";
-			std::cout << "\n";
-		}
-	}
+	//for (int i = 0; i < knnMetrics.size(); ++i)
+	//{
+	//	Metrics metrics = knnMetrics[i];
+	//	std::cout << "\n\nKNN metrics for k = " << knnKList[i] << " :\n";
+	//	std::cout << "Confusion matrix :\n";
+	//	PrintConfusionMatrix(metrics.confusionMatrix);
+	//	std::cout << "Accuracy : " << metrics.accuracy << "%; " << "time : " << metrics.time * 1000 << " ms\n";
+	//	for (int j = 0; j < metrics.classesPrecisionRecallF1.size(); ++j)
+	//	{
+	//		std::cout << "class " << j + 1 << "\n";
+	//		std::cout << "Precision : " << metrics.classesPrecisionRecallF1[j].precision * 100 << "%\t";
+	//		std::cout << "Recall : " << metrics.classesPrecisionRecallF1[j].recall * 100 << "%\t";
+	//		std::cout << "f1 score : " << metrics.classesPrecisionRecallF1[j].f1Score * 100 << "%\t";
+	//		std::cout << "\n";
+	//	}
+	//}
 	return 0;
 }
